a50d13dbd8b25886d25e9ce822aa77cfa2f65f4b
gabriel-bgsantos <100386033+gabriel-bgsantos@users.noreply.github.com>
2022-02-25 04:35:20 -0300

    Update transference.py

--- transference.py
+++ transference.py
@@ -16,6 +16,6 @@ def get_that_money(amount):
 
     for transfer in transfers:        
             if "created" in transfer.status:
-                print("Transference completed!\n", transfer)
+                print("The transfer was completed!\n", transfer)
             else:
-                print ("There was an error, the transference couldn't be completed...")
+                print ("There was an error, the transfer couldn't be completed...")
