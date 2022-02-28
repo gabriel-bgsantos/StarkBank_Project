74cedf818c1f07914839d115457cefa21e7b7fda
gabriel-bgsantos <100386033+gabriel-bgsantos@users.noreply.github.com>
2022-02-27 21:50:38 -0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import starkbank
 import transference
+import events_info
 
 def url_webhooks(): 
     webhook = starkbank.webhook.create(     #receiving the webhook events through the link below
@@ -11,7 +12,10 @@ def url_webhooks():
     )
 
 def webhooks():         #transfers the right amount to everyone with the "credited" status
-    events = starkbank.event.query(after=datetime.now())
+    events = starkbank.event.query(after=datetime.utcnow())
     for event in events:
             if event.log.type == "credited": 
-                transference.get_that_money(event.log.invoice.amount) 
+                transference.get_that_money(event.log.invoice.amount)
+                print("Event ID: ",[event]," Transfer requested, amount: ", event.log.invoice.amount) 
+
+    events_info.first_validation()
