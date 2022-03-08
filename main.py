bd230d92f125dc280d75c782d4a90e5c2e8c7078
Gabriel Batista <100386033+gabriel-bgsantos@users.noreply.github.com>
2022-03-08 19:17:50 -0300

    Update main.py
    Fixed the Webhook's URL error that was occurring after the first loop and improved the output

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ import time
 def main():
     timing = 8 #24 hours because at each loop 1 is out until it reaches 0, then it stops
     while timing != 0:
+        print("--- New loop starting ---")
         keys.authenticator()
         invoices.create_invoices()
-        webhook.url_webhooks()
+        
+        #the if below prevents the asking of new Webhook URLs when in another loop (which throws the "URL is already being used" error)
+        if timing == 8:
+            webhook.url_webhooks()
+        else:
+            pass
+        
         webhook.webhooks()
         timing -= 1 #one is out at each loop, so the loops don't run forever
+        print("Finishing this loop. Loops remaining:", timing,"\nJust wait...\n")
         time.sleep(10800)   #each loop runs only every 3 hours
-
+    
+    print("All loops have ended, the program is being closed...")
 
 if __name__ == '__main__':
         main()
